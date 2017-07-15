d5402ae1c49b9640a30126b540b406fbe6e9976c
Connor Dodge <connor.m.dodge@gmail.com>
2017-07-15 07:45:58 -0600

    remove onLevelWasLoaded, added SceneManager.sceneLoaded

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour {
 	public AudioClip[] levelMusicChangeArray;
 
 	private AudioSource audioSource;
 	// Use this for initialization
+
+	void OnEnable(){
+	//Tell our 'OnSceneLoaded' function to start listening for a scene change as soon as this script is enabled.
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	void OnDisable(){
+	//Tell our 'OnSceneLoaded' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode){
+		AudioClip thisLevelsMusic = levelMusicChangeArray[scene.buildIndex];
+		if (thisLevelsMusic){
+			audioSource.clip = thisLevelsMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+
 	void Awake () {
 		DontDestroyOnLoad (gameObject);
 	}
@@ -14,14 +35,5 @@ public class MusicManager : MonoBehaviour {
 	void Start()
 	{
 		audioSource = GetComponent<AudioSource>();
-	}
-	
-	void OnLevelWasLoaded (int level) {
-		AudioClip thisLevelsMusic = levelMusicChangeArray[level];
-		if (thisLevelsMusic){
-			audioSource.clip = thisLevelsMusic;
-			audioSource.loop = true;
-			audioSource.Play();
-		}
 	}
 }
