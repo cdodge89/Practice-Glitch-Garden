497448c00b8c7f9f9bd7e5f22a17532167c62cb9
Connor Dodge <connor.m.dodge@gmail.com>
2017-07-19 16:44:31 -0600

    added player prefs manager

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerPrefsManager : MonoBehaviour {
 
 	const string MASTER_VOLUME_KEY = "master_volume";
-	const string DIFFICULTY = "difficulty";
+	const string DIFF_KEY = "difficulty";
 	const string LEVEL_KEY = "level_unlocked_";
 	//level_unlocked_1 etc
 
@@ -13,11 +14,46 @@ public class PlayerPrefsManager : MonoBehaviour {
 		if (volume >= 0f && volume <= 1f ){
 			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
 		} else {
-			Debug.LogError ('Master Volume out of range');
+			Debug.LogError ("Master Volume out of range");
 		}
 	}
 
 	public static float GetMasterVolume(){
 		return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+	}
+
+	public static void UnlockLevel(int level){
+		if(level <= SceneManager.sceneCountInBuildSettings - 1){
+			PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1); //use 1 for true
+		} else {
+			Debug.LogError ("Cannot unlock level " + level + ". It is not in the build order");
+		}
+	}
+
+	public static bool IsLevelUnlocked (int level){
+		if(level <= SceneManager.sceneCountInBuildSettings - 1){
+			int levelValue = PlayerPrefs.GetInt(LEVEL_KEY + level.ToString());
+			if(levelValue == 1){
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			Debug.LogError("Cannot query level " + level + ". It is not in the build order");
+			return false;
+		}
+	}
+
+	public static void SetDifficulty(float difficulty){
+		if(difficulty >= 0f && difficulty <= 1f){
+			PlayerPrefs.SetFloat(DIFF_KEY, difficulty);
+		} else {
+			Debug.LogError("Difficulty must be between 0 and 1");
+		}
+	}
+
+	public static float GetDifficulty(){
+		float difficulty = PlayerPrefs.GetFloat(DIFF_KEY);
+		return difficulty;
 	}
 }
